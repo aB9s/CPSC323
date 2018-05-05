59cf1b25d3b2e74852f2ae1697afa4380afc5015
aB9 <abhi09.mu@gmail.com>
2018-05-04 20:21:58 -0700

    Resolved all known bugs from Assignment 2

--- lexer.py
+++ lexer.py
@@ -5,156 +5,166 @@
 ##                          2. Abhishek Mhatre                     ##
 ##                                                                 ##
 #####################################################################
-from _dummy_thread import _main
 
 
-#Keywords
-Keywords   = ["int", "float", "real", "boolean" ,"if", "else", "endif", "while", "return", "get", "put","function","for"]
-#Separators
-Separators = ["{", "}", "[", "]", "(" ,")", ";", ",",":"]
-#Operators
-Operators  = ["+", "-", "*", "/", ">", "<", "=","!","&" ,"^"]
- 
+# Keywords
+Keywords = ["int", "float", "real", "boolean", "if", "else", "endif", "while", "return", "get", "put", "function",
+            "for"]
+# Separators
+Separators = ["{", "}", "[", "]", "(", ")", ";", ",", ":"]
+# Operators
+Operators = ["+", "-", "*", "/", ">", "<", "=", "!", "&", "^"]
+
+# Double Operators
+DoubleOperators = ["=","<",">","^"]
+
 # File name without extension
 source_filename = ""
 # Machine States
 buffer_token = ""
-Token_Array  = []
+Token_Array = []
 Lexeme_Array = []
-Buffer_Array =[]
+Buffer_Array = []
 Line_Number_Array = []
 token = ""
 lexeme = ""
- 
-def crateBuffer(char, e_o_l):
+
+
+def create_buffer(char, e_o_l):
     global buffer_token
-    #identifier/ keyword/ integer/ real
+    # identifier/ keyword/ integer/ real
     if char.isalpha() or char.isdigit():
-        if buffer_token !="" and buffer_token[-1] in Operators:
+        if buffer_token != "" and buffer_token[-1] in Operators:
             Buffer_Array.append(buffer_token)
             buffer_token = ""
-        buffer_token +=char
-    #identifier (last letter of the identifier should be a letter or a $ sign)
-    elif char =="$":                      
-        buffer_token+=char
-    #all characters in the buffer are digit and '.' (dot) appeared--> possible real
-    elif buffer_token.isdigit() and char ==".":       
-        buffer_token+=char
-    #Separator
-    elif char in Separators:            
+        buffer_token += char
+    # identifier (last letter of the identifier should be a letter or a $ sign)
+    elif char == "$":
+        buffer_token += char
+    # all characters in the buffer are digit and '.' (dot) appeared--> possible real
+    elif buffer_token.isdigit() and char == ".":
+        buffer_token += char
+    # Separator
+    elif char in Separators:
         Buffer_Array.append(buffer_token)
         Buffer_Array.append(char)
         buffer_token = ""
-    #Operator
-    elif char in Operators:                     
-        if buffer_token =="":
-            buffer_token+=char
-#             Buffer_Array.append(buffer_token)
-        #already an operator present in the buffer --> could be a double operator
-#         elif buffer_token != "" and buffer_token[-1] in Operators:     
-#             buffer_token+=char
+    # Operator
+    elif char in Operators:
+        if buffer_token == "":
+            buffer_token += char
+        #             Buffer_Array.append(buffer_token)
+        # already an operator present in the buffer --> could be a double operator
+        #         elif buffer_token != "" and buffer_token[-1] in Operators:
+        #             buffer_token+=char
         elif buffer_token[-1].isalpha() or buffer_token[-1].isdigit():
             Buffer_Array.append(buffer_token)
             buffer_token = char
         else:
-            buffer_token+=char
+            buffer_token += char
             Buffer_Array.append(buffer_token)
             buffer_token = ""
     # Unknown
-    elif char==" " or char=="\n" or char =="\t":        
-#         if char =="!":
-#             buffer_token+= char
+    elif char == " " or char == "\n" or char == "\t":
+        #         if char =="!":
+        #             buffer_token+= char
         Buffer_Array.append(buffer_token)
-        buffer_token =""
+        buffer_token = ""
     else:
-#         if char =="!":
-#             buffer_token+= char
-        buffer_token+=char
-#         Buffer_Array.append(buffer_token)
-#         Buffer_Array.append(char)
+        #         if char =="!":
+        #             buffer_token+= char
+        buffer_token += char
+    #         Buffer_Array.append(buffer_token)
+    #         Buffer_Array.append(char)
 
-    #End of line reached
+    # End of line reached
     if e_o_l:
         Buffer_Array.append(buffer_token)
-        buffer_token =""
-    
-        
+        buffer_token = ""
 
-        
-    
     ###Function to # 
-def lexer(Buffer_Array,i):
+
+
+def lexer(Buffer_Array, i):
     global token, lexeme
     line_number = i
-    
+
     for char in Buffer_Array:
-       #If its an empty space, just ignore it
+        # If its an empty space, just ignore it
         if len(char) > 0:
-            #Process
+            # Process
             for c in char:
-                #Beginning of comment section
-                if c =="!" and (token =="Unknown" or token ==""):
+                # Beginning of comment section
+                if c == "!" and (token == "Unknown" or token == ""):
                     token = "Comment"
-                    #End of the comment section
-                elif c =="!" and token =="Comment":
-                    token =""
-                #Identifier/ Keyword
+                    # End of the comment section
+                elif c == "!" and token == "Comment":
+                    token = ""
+                # Double Operator
+                elif token =="Operator" and c in DoubleOperators:
+                    Token_Array.append(token)
+                    token = ""
+                    lexeme += c
+                    Lexeme_Array.append(lexeme)
+                    lexeme = ""
+                    Line_Number_Array.append(line_number)
+                # Identifier/ Keyword
                 elif c.isalpha():
-                    #Its a part of comment section, ignore it
-                    if token =="Comment":
+                    # Its a part of comment section, ignore it
+                    if token == "Comment":
                         break
-                    elif token=="" or token=="Identifier":
-                        lexeme +=c
+                    elif token == "" or token == "Identifier":
+                        lexeme += c
                         token = "Identifier"
-                #Identifier
-                elif c =="$" and token =="Identifier":
-                     #Its a part of comment section, ignore it
-                    if token =="Comment":
+                # Identifier
+                elif c == "$" and token == "Identifier":
+                    # Its a part of comment section, ignore it
+                    if token == "Comment":
                         break
                     else:
-                        lexeme +=c
+                        lexeme += c
                         token = "Identifier"
-                #Integer/ Real
+                # Integer/ Real
                 elif c.isdigit():
-                     #Its a part of comment section, ignore it
-                    if token =="Comment":
+                    # Its a part of comment section, ignore it
+                    if token == "Comment":
                         break
-                    elif token != "" and token =="Identifier":
-                        lexeme+=c
-                    
-                    elif token =="" or token =="Integer":
-                        lexeme+=c
-                        token = "Integer"                        
-                    elif token =="Real":
-                        lexeme +=c
+                    elif token != "" and token == "Identifier":
+                        lexeme += c
+
+                    elif token == "" or token == "Integer":
+                        lexeme += c
+                        token = "Integer"
+                    elif token == "Real":
+                        lexeme += c
                         token = "Real"
                     else:
-                        lexeme +=c
+                        lexeme += c
                         token = "Unknown"
                 # Real
-                elif c ==".":
-                     #Its a part of comment section, ignore it
-                    if token =="Comment":
+                elif c == ".":
+                    # Its a part of comment section, ignore it
+                    if token == "Comment":
                         break
-                    #Its a separator or an unknown
-                    elif token == "Identifier" or token =="Unknown":
+                    # Its a separator or an unknown
+                    elif token == "Identifier" or token == "Unknown":
                         Token_Array.append(token)
                         Lexeme_Array.append(lexeme)
                         Line_Number_Array.append(line_number)
                         token = ""
-                        lexeme= ""
-                    #its a real value
-                    elif token == "Integer":                                
-                        lexeme +=c
+                        lexeme = ""
+                    # its a real value
+                    elif token == "Integer":
+                        lexeme += c
                         token = "Real"
                     else:
-#                         Token_Array.append(token)
-#                         Lexeme_Array.append(lexeme)
+                        #                         Token_Array.append(token)
+                        #                         Lexeme_Array.append(lexeme)
                         token = "Unknown"
-                        lexeme+= c
+                        lexeme += c
                 elif c in Separators:
-                     #Its a part of comment section, ignore it
-                    if token =="Comment":
+                    # Its a part of comment section, ignore it
+                    if token == "Comment":
                         break
                     else:
                         token = "Separator"
@@ -165,51 +175,51 @@ def lexer(Buffer_Array,i):
                         lexeme = ""
                         Line_Number_Array.append(line_number)
                 elif c in Operators:
-                     #Its a part of comment section, ignore it
-                    if token =="Comment":
+                    # Its a part of comment section, ignore it
+                    if token == "Comment":
                         break
-                    elif token=="":
+                    elif token == "":
                         token = "Operator"
-                        Token_Array.append(token)
-                        token = ""
+                        # Token_Array.append(token)
+                        # token = ""
                         lexeme += c
-                        Lexeme_Array.append(lexeme)
-                        lexeme = ""
-                        Line_Number_Array.append(line_number)
+                        # Lexeme_Array.append(lexeme)
+                        # lexeme = ""
+                        # Line_Number_Array.append(line_number)
                 else:
-                    token= "Unknown"
-                    lexeme +=c 
-                    
-            if token =="Identifier" and lexeme in Keywords:
+                    token = "Unknown"
+                    lexeme += c
+
+            if token == "Identifier" and lexeme in Keywords:
                 token = "Keyword"
                 Token_Array.append(token)
                 token = ""
                 Lexeme_Array.append(lexeme)
                 lexeme = ""
                 Line_Number_Array.append(line_number)
-#             elif token =="Identifier" or token=="Integer" or token=="Real" or token =="Separator" or token =="Operator" or token =="Unknown":
+            #             elif token =="Identifier" or token=="Integer" or token=="Real" or token =="Separator" or token =="Operator" or token =="Unknown":
             else:
-                if token=="Identifier":
-                    if lexeme[-1] ==".":
+                if token == "Identifier":
+                    if lexeme[-1] == ".":
                         Token_Array.append("Unknown")
                         token = ""
                         Lexeme_Array.append(lexeme)
-                        lexeme = "" 
+                        lexeme = ""
                         Line_Number_Array.append(line_number)
-                    elif lexeme[-1].isalpha() or lexeme[-1] =="$":
+                    elif lexeme[-1].isalpha() or lexeme[-1] == "$":
                         Token_Array.append(token)
                         token = ""
                         Lexeme_Array.append(lexeme)
-                        lexeme= ""
+                        lexeme = ""
                         Line_Number_Array.append(line_number)
                     else:
                         Token_Array.append("Unknown")
                         token = ""
                         Lexeme_Array.append(lexeme)
-                        lexeme= ""
+                        lexeme = ""
                         Line_Number_Array.append(line_number)
-                elif token =="Integer" or token =="Real":
-                    if lexeme[-1] ==".":
+                elif token == "Integer" or token == "Real":
+                    if lexeme[-1] == ".":
                         Token_Array.append("Unknown")
                         token = ""
                         Lexeme_Array.append(lexeme)
@@ -219,32 +229,32 @@ def lexer(Buffer_Array,i):
                         Token_Array.append(token)
                         token = ""
                         Lexeme_Array.append(lexeme)
-                        lexeme= ""
+                        lexeme = ""
                         Line_Number_Array.append(line_number)
-                elif token =="Comment":
+                elif token == "Comment":
                     pass
                 else:
                     Token_Array.append(token)
                     token = ""
                     Lexeme_Array.append(lexeme)
-                    lexeme= ""
+                    lexeme = ""
                     Line_Number_Array.append(line_number)
-              
-            
+
+
         else:
-            if token =="Identifier" and lexeme in Keywords:
+            if token == "Identifier" and lexeme in Keywords:
                 Token_Array.append(token)
                 token = ""
                 Lexeme_Array.append(lexeme)
                 lexeme = ""
                 Line_Number_Array.append(line_number)
-            elif token =="Identifier" or token =="Unknown":
+            elif token == "Identifier" or token == "Unknown":
                 Token_Array.append(token)
                 token = ""
                 Lexeme_Array.append(lexeme)
-                lexeme= ""
+                lexeme = ""
                 Line_Number_Array.append(line_number)
-            elif token =="Comment":
+            elif token == "Comment":
                 pass
             else:
                 Token_Array.append(token)
@@ -252,64 +262,66 @@ def lexer(Buffer_Array,i):
                 Lexeme_Array.append(lexeme)
                 lexeme = ""
                 Line_Number_Array.append(line_number)
-                
-    #Clear the buffer for the next line
+
+    # Clear the buffer for the next line
     Buffer_Array.clear()
-    
+
     #### End of lexer() ####
-     
-             
-            
+
     #### Function to print Tokens and Lexemes table into a file ####    
+
+
 def print_to_file():
-    outputFile = open(source_filename+"_lexer_output" + ".txt", "w")
-    outputFile.write("Line Number"+ "\t\t\t" +"Token" + "\t\t\t" + "Lexeme\n")
+    outputFile = open(source_filename + "_lexer_output" + ".txt", "w")
+    outputFile.write("Line Number" + "\t\t\t" + "Token" + "\t\t\t" + "Lexeme\n")
     outputFile.write("----------------------------------------------------\n")
     i = 0
     while i < len(Token_Array):
         if Token_Array[i] != "":
-            if Token_Array[i] == "Keyword" or Token_Array == "Unknown" or Token_Array[i] == "Integer" or Token_Array[i] == "Real":
-                outputFile.write(str(Line_Number_Array[i])+ "\t\t\t\t\t" + Token_Array[i] + "\t\t\t" + Lexeme_Array[i] + "\n")
-                
+            if Token_Array[i] == "Keyword" or Token_Array == "Unknown" or Token_Array[i] == "Integer" or Token_Array[
+                i] == "Real":
+                outputFile.write(
+                    str(Line_Number_Array[i]) + "\t\t\t\t\t" + Token_Array[i] + "\t\t\t" + Lexeme_Array[i] + "\n")
+
             else:
-                outputFile.write(str(Line_Number_Array[i])+ "\t\t\t\t\t" + Token_Array[i] + "\t\t" + Lexeme_Array[i] + "\n")
+                outputFile.write(
+                    str(Line_Number_Array[i]) + "\t\t\t\t\t" + Token_Array[i] + "\t\t" + Lexeme_Array[i] + "\n")
         i += 1
     outputFile.close()
-    
-    
+
     #### End of print_to_file() ####
-         
+
+
 def main():
     global source_filename
-    #Get file 
+    # Get file
     filename = input("Enter name of source file to be parsed: ")
     file = open(filename, "r")
-    #Remove extension from the source filename
+    # Remove extension from the source filename
     source_filename = filename.split(".")[0]
-    
+
     source_code_text = file.readlines()
-    
+
     line_num = 1
     for line in source_code_text:
         i = 0
-        
+
         while i < len(line):
-            #End of line
-            e_o_l = True if i ==(len(line)-1) else False
-            #Read each token from the source code line and add it into a buffer
-            crateBuffer(line[i],e_o_l)
+            # End of line
+            e_o_l = True if i == (len(line) - 1) else False
+            # Read each token from the source code line and add it into a buffer
+            create_buffer(line[i], e_o_l)
             i += 1
-    
-        #Sort tokens into lexemes
-        lexer(Buffer_Array,line_num)
-        line_num+=1
-    
-    
-    #Print table to a file 
+
+        # Sort tokens into lexemes
+        lexer(Buffer_Array, line_num)
+        line_num += 1
+
+    # Print table to a file
     print_to_file()
- 
-    #Close the file
+
+    # Close the file
     file.close()
-    
+
     return source_filename
     #### End of main() ####
