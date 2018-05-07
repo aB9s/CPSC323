14432ca3a0f96e7dac5f51f78e6ef6fba5f4ee12
aB9 <abhi09.mu@gmail.com>
2018-05-06 19:28:10 -0700

    removed unnecessary files

---
+++ Stack.py
@@ -0,0 +1,27 @@
+"""
+                          
+    FileName:   Stack       
+    Date:       5/6/18 4:10 PM   
+    Author:     aB9           
+                                
+"""
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
